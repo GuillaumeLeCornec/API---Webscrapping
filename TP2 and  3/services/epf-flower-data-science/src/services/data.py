61644137cea32e2afc7ca31074b436a3aaa10bcd
GuillaumeLeCornec <guillaume.lecornec@epfedu.fr>
2024-12-11 19:16:12 +0100

    step13 done in firestore

--- TP2 and  3/services/epf-flower-data-science/src/services/data.py
+++ TP2 and  3/services/epf-flower-data-science/src/services/data.py
@@ -254,19 +254,4 @@ def get_pred(train_test_dataset, training_model):
     
     return y_pred
     
-def create_firestore_parameters():
-    # Initialiser le client Firestore
-    db = firestore.Client()
 
-    # Créer la collection "parameters" et insérer le document "parameters"
-    parameters_data = {
-        "n_estimators": 100,
-        "criterion": "gini"
-    }
-
-    # Ajouter le document dans la collection
-    db.collection("parameters").document("parameters").set(parameters_data)
-    print("Parameters document created successfully.")
-    collection_ref = db.collection("parameters")
-    docs = collection_ref.stream()
-    return docs
